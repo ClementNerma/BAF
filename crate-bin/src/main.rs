a13c2551c1dae4a8eefdda663692453f0ecd2509
Clément Nerma <clement.nerma@gmail.com>
2026-03-23 22:04:54 +0100

    refactor: move fns to utils module

--- crate-bin/src/main.rs
+++ crate-bin/src/main.rs
@@ -21,11 +21,13 @@ use self::{
     args::{Action, CmdArgs},
     logger::Logger,
     tree::ArchiveContentTree,
+    utils::human_size,
 };
 
 mod args;
 mod logger;
 mod tree;
+mod utils;
 
 fn main() -> ExitCode {
     let args = CmdArgs::parse();
@@ -312,56 +314,4 @@ fn get_item_mtime(path: &Path) -> Result<Timestamp> {
             });
 
     Ok(Timestamp::from(mtime))
-}
-
-pub fn human_size(size: u64, precision: Option<u8>) -> String {
-    let units = ["B", "KiB", "MiB", "GiB", "TiB"];
-
-    let (unit, unit_base) = units
-        .iter()
-        .enumerate()
-        .rev()
-        .find_map(|(i, unit)| {
-            let base = 1024_u64.pow(i.try_into().unwrap());
-
-            if size >= base || base == 1 {
-                Some((unit, base))
-            } else {
-                None
-            }
-        })
-        .unwrap();
-
-    format!(
-        "{} {unit}",
-        approx_int_div(size, unit_base, precision.unwrap_or(2))
-    )
-}
-
-/// Perform an approximate integer division
-///
-/// The last decimal will be rounded to the nearest.
-///
-/// The `precision` parameter is the number of floating-point decimals to keep.
-pub fn approx_int_div(a: u64, b: u64, precision: u8) -> String {
-    let max_prec = 10_u128.pow(u32::from(precision));
-
-    let div = u128::from(a) * max_prec * 10 / u128::from(b);
-    let div = (div / 10) + if div % 10 >= 5 { 1 } else { 0 };
-
-    let int_part = div / max_prec;
-    let frac_part = div % max_prec;
-
-    let mut out = int_part.to_string();
-
-    if frac_part > 0 && precision > 0 {
-        out.push('.');
-        out.push_str(&format!(
-            "{:#0precision$}",
-            frac_part,
-            precision = precision.into()
-        ));
-    }
-
-    out
 }
